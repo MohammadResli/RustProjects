3de2e0c59349bdb49898b463a6dfbca2c0b310bf
MohammadResli <mohammad.resli@gmail.com>
2022-09-03 12:40:55 +0300

    added the score feature to the game.

--- GuessingGames/guessing_game_01/src/main.rs
+++ GuessingGames/guessing_game_01/src/main.rs
@@ -6,9 +6,17 @@ fn main() {
     println!("Welcome to Guess-Game-01\n In this Game there is a hidden number and you have to guess it.");
 
     let hidden_number: i32 = rand::thread_rng().gen_range(1..=100);
+    
+    // score is calculated based on the fomula score = max(0,101-number of guesses);
+    let mut score :i32;
+    let mut number_of_guesses: i32 = 0;
 
     loop {
         println!("Please Enter your guess:");
+
+        number_of_guesses += 1;
+        
+        score = std::cmp::max(0,101-number_of_guesses);
 
         let mut guess = String::new();
 
@@ -23,14 +31,24 @@ fn main() {
                 continue;
             }
         };
+        let mut suffix = "th";
+        if number_of_guesses%10==1{
+            suffix = "st";
+        }
+        if number_of_guesses%10==2{
+            suffix = "nd";
+        }
+        if number_of_guesses%10==3{
+            suffix = "rd";
+        }
 
-        println!("You guessed {guess}");
+        println!("Your {number_of_guesses}{suffix} guessed-number is: {guess}");
 
         match guess.cmp(&hidden_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!!");
+                println!("You win!!\nYour score is: {score}");
                 break;
             }
         }
