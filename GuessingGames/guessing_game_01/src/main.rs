a5d49276a33ab1522bf249f586211dad1461f9d3
MohammadResli <mohammad.resli@gmail.com>
2022-09-03 13:20:44 +0300

    Added the difficulty level to the game.

--- GuessingGames/guessing_game_01/src/main.rs
+++ GuessingGames/guessing_game_01/src/main.rs
@@ -5,18 +5,46 @@ use std::io;
 fn main() {
     println!("Welcome to Guess-Game-01\n In this Game there is a hidden number and you have to guess it.");
 
+    println!("There is three levels available for this game: ");
+    println!("-Easy: the number of allowed guesses to 100.");
+    println!("-Medium: the number of allowed guesses to 50.");
+    println!("-Hard: the number of allowed guesses to 10.");
+    println!("Default level is: Medium (50 allowed guesses).");
+    println!(
+        "Please Enter the desired level for the game: [E for Easy, M for Medium, H for Hard]."
+    );
+    let mut level = String::new();
+    io::stdin()
+        .read_line(&mut level)
+        .expect("Failed to read line!");
+
+    let level = level.trim();
+    println!("Level: {}", level);
+    let allowed_guesses: i32 = match level {
+        "E" => 100,
+        "M" => 50,
+        "H" => 10,
+        _ => 50,
+    };
+
+    println!("The number of allowed guesses for this game is: {allowed_guesses}");
+
     let hidden_number: i32 = rand::thread_rng().gen_range(1..=100);
-    
+
     // score is calculated based on the fomula score = max(0,101-number of guesses);
-    let mut score :i32;
+    let mut score: i32;
     let mut number_of_guesses: i32 = 0;
 
     loop {
+        if number_of_guesses >= allowed_guesses {
+            println!("You Lose!! because you don't have any allowed guesses any more.");
+            break;
+        }
         println!("Please Enter your guess:");
 
         number_of_guesses += 1;
-        
-        score = std::cmp::max(0,101-number_of_guesses);
+
+        score = std::cmp::max(0, 101 - number_of_guesses);
 
         let mut guess = String::new();
 
@@ -32,13 +60,13 @@ fn main() {
             }
         };
         let mut suffix = "th";
-        if number_of_guesses%10==1{
+        if number_of_guesses % 10 == 1 {
             suffix = "st";
         }
-        if number_of_guesses%10==2{
+        if number_of_guesses % 10 == 2 {
             suffix = "nd";
         }
-        if number_of_guesses%10==3{
+        if number_of_guesses % 10 == 3 {
             suffix = "rd";
         }
 
